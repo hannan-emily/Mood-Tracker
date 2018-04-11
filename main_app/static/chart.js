17b17485986ff26c925f2e72ba8fe6337f7e1a88
Chelsea Zhu <qianwei5668@gmail.com>
2018-04-11 14:36:44 -0700

    adding all charts including sample page

--- main_app/static/chart.js
+++ main_app/static/chart.js
@@ -1,3 +1,6 @@
+
+
+
 color_dictionary = [
     
     "#F0D5E3",
@@ -20,16 +23,107 @@ color_dictionary = [
 document.addEventListener('DOMContentLoaded', function() {
 
     bgcolor = color_dictionary.slice(0,values.length)
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
+    var ctx1 = document.getElementById("pieChart").getContext('2d');
+    var pieChart = new Chart(ctx1, {
       type: 'pie',
       data: {
         labels: labels,
         datasets: [{
-          backgroundColor: 
-          bgcolor,
+          backgroundColor: bgcolor,
           data: values
         }]
       }
     });
+
+    var ctx2 = document.getElementById("barChart").getContext('2d');
+    var options = {
+        scales: {
+            xAxes: [{
+                display: true,
+                gridLines: {
+                    offsetGridLines: false
+                }
+            }],
+            yAxes: [{
+                display: false,
+                ticks: {
+                  min: 0
+                },
+                gridLines: {
+                    offsetGridLines: false
+                }
+            }]
+        }
+    }
+    var barChart = new Chart(ctx2, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'mood',
+          backgroundColor: bgcolor,
+          data: values
+        }]
+      },
+      options: options
+
+    });
+
+
+    var ctx3 = document.getElementById("samplePieChart").getContext('2d');
+    var pieChart = new Chart(ctx3, {
+      type: 'pie',
+      data: {
+        labels: ['happy', 'sad', 'disgusted', 'depressed', 'excited', 'surprised'],
+        datasets: [{
+          backgroundColor: color_dictionary.slice(0,6),
+          data: [15, 4, 1, 1, 7, 3]
+        }]
+      }
+    });
+
+    var ctx4 = document.getElementById("sampleBarChart").getContext('2d');
+    var options = {
+        scales: {
+            xAxes: [{
+                display: true,
+                gridLines: {
+                    offsetGridLines: false
+                }
+            }],
+            yAxes: [{
+                display: false,
+                ticks: {
+                  min: 0
+                },
+                gridLines: {
+                    offsetGridLines: false
+                }
+            }]
+        }
+    }
+    var barChart = new Chart(ctx4, {
+      type: 'bar',
+      data: {
+        labels: ['happy', 'sad', 'disgusted', 'depressed', 'excited', 'surprised'],
+        datasets: [{
+          label: 'mood',
+          backgroundColor: color_dictionary.slice(0,6),
+          data: [15, 4, 1, 1, 7, 3]
+        }]
+      },
+      options: options
+
+    });
+
+
 })
+
+
+
+
+
+
+
+
+
