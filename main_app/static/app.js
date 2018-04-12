3c214dadb3cd4b4daab11fad8652233eafa1da7b
hannan-emily <emilyhannan@gmail.com>
2018-04-12 11:59:46 -0700

    explanatory content added to how it works page, and home page

--- main_app/static/app.js
+++ main_app/static/app.js
@@ -53,29 +53,7 @@ $(document).ready(function(){
   ]
 });
 
-// $('.js-remove-slide').slick('slickRemove', slideIndex - 1);
 
-$('.js-remove-slide').on('click', function() {
-  $('.responsive').slick('slickRemove', $('.slick-slide').index(this) - 1)
-});
 
-// Get the current slide
-// var currentSlide = $('.your-element').slick('slickCurrentSlide');
-//
-// $('.add-remove').slick({
-//   slidesToShow: 3,
-//   slidesToScroll: 3
-// });
-// $('.js-add-slide').on('click', function() {
-//   slideIndex++;
-//   $('.add-remove').slick('slickAdd','<div><h3>' + slideIndex + '</h3></div>');
-// });
-//
-// $('.js-remove-slide').on('click', function() {
-//   $('.add-remove').slick('slickRemove',slideIndex - 1);
-//   if (slideIndex !== 0){
-//     slideIndex--;
-//   }
-// });
 
 });
